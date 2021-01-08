3d7c146710fde974f6aca0d63b4698d7aaa2f4a6
mikzur <mikzur4@gmail.com>
2021-01-08 01:28:20 +0100

    change Launcher so it makes game with Constants.width and height

--- src/dungeoncrawler/Launcher.java
+++ src/dungeoncrawler/Launcher.java
@@ -1,13 +1,10 @@
 package dungeoncrawler;
 
 
-import javax.swing.*;
-
 public class Launcher{
 
     public static void main(String[] args){
         //GameWindow display = new GameWindow(800, 480);
-        new Thread(new GameLoop(new Game(800,400))).start();
+        new Thread(new GameLoop(new Game(Constants.SCR_WIDTH, Constants.SCR_HEIGHT))).start();
     }
-
 }
