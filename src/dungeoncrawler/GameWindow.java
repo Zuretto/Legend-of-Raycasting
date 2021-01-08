1e0bea141ad4e155782d67620a47f2a4cd07ec0c
mikzur <mikzur4@gmail.com>
2021-01-08 01:30:02 +0100

    add renderWorld with textures

--- src/dungeoncrawler/GameWindow.java
+++ src/dungeoncrawler/GameWindow.java
@@ -1,23 +1,35 @@
 package dungeoncrawler;
 
+import dungeoncrawler.gameObjects.Input;
+import dungeoncrawler.gameObjects.Team;
+
 import java.awt.*;
+import java.awt.event.KeyListener;
 import java.awt.image.BufferStrategy;
+import java.io.IOException;
 import javax.swing.JFrame;
 
 public class GameWindow extends JFrame {
 
+    private GameWindowResourceManager resourceManager;
     private Canvas canvas;
 
-    public GameWindow(int width, int height) {
+    public GameWindow(int width, int height, Input input) {
 
         setTitle("Java project");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
         setSize(width, height);
 
+        try {
+            this.resourceManager = new GameWindowResourceManager();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         canvas = new Canvas();
         canvas.setPreferredSize((new Dimension(width,height)));
         canvas.setFocusable(false);
+        addKeyListener((KeyListener) input);
         add(canvas);
         pack();
 
@@ -27,66 +39,52 @@ public class GameWindow extends JFrame {
         setVisible(true);
     }
 
-    public void render(Game game){
+    public void renderWorld(Team team, Map map, double[] ZBuffer){
         BufferStrategy bufferStrategy = canvas.getBufferStrategy();
         Graphics graphics = bufferStrategy.getDrawGraphics();
-
-        graphics.setColor(Color.BLACK);
-        graphics.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
-        Rectangle rectangle = game.getRectangle();
         graphics.setColor(Color.blue);
-        graphics.fillRect((int) rectangle.getX(), (int)rectangle.getY(),
-                          (int) rectangle.getWidth(), (int) rectangle.getHeight()
-        );
-        /*
-        sf::RectangleShape sky(sf::Vector2f(casterWidth, casterHeight / 2));
-        sky.setFillColor(sf::Color(108, 158, 222));
-        target.draw(sky);
-        sf::RectangleShape floor(sf::Vector2f(casterWidth, casterHeight / 2));
-        floor.move(0, casterHeight/2);
-        floor.setFillColor(sf::Color(56, 68, 82));
-        target.draw(floor);
-        auto map_vect = worldMap->getMapVector();
-        for(int x = 0; x < casterWidth; x++){
-            //calculate ray position and direction
-            double cameraX = 2 * x / (double)casterWidth - 1; //-1 for left pixel, 1 for right pixel
-            double rayDirX = player->getdirX() + player->getplaneX() * cameraX;
-            double rayDirY = player->getdirY() + player->getplaneY() * cameraX;
-            //which box of the map we're in
-            int mapX = int(player->getPosX());
-            int mapY = int(player->getPosY());
-            // length of ray from one x or y-side to next x or y-side
-            // "1" is for the length of vector [rayDirX, rayDirY].
-            double deltaDistX = (rayDirY == 0) ? 0 : ((rayDirX == 0) ? 1 : std::abs(1 / rayDirX));
-            double deltaDistY = (rayDirX == 0) ? 0 : ((rayDirY == 0) ? 1 : std::abs(1 / rayDirY));
-            //length of ray from current position to next x or y-side
+        graphics.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
+        for(int stripeX = 0; stripeX < this.getWidth(); ++stripeX){
+            //calclate ray position and direction
+            double cameraX = 2 * stripeX / (double) this.getWidth() - 1; //-1 for left pixel, 1 for right pixel
+            double rayDirX = team.getDirX() + team.getPlaneX() * cameraX;
+            double rayDirY = team.getDirY() + team.getPlaneY() * cameraX;
+            //mapX and mapY - X and Y coordinates of team on the map
+            int mapX = (int)team.getPosX();
+            int mapY = (int)team.getPosY();
+            //length of the ray from one X or Y-side to next X, Y sides
+            //double deltaDistX = (rayDirX == 0) ? 0 : ((rayDirY == 0) ? 1 : Math.abs(1.0 / rayDirX));
+            //double deltaDistY = (rayDirX == 0) ? 0 : ((rayDirY == 0) ? 1 : Math.abs(1.0 / rayDirY));
+            double deltaDistX = Math.abs(1.0 / rayDirX);
+            double deltaDistY = Math.abs(1.0 / rayDirY);
+            //length of ray from CURRENT position to next x, y sides
             double sideDistX;
             double sideDistY;
-            //what direction to step in x or y-direction (either +1 or -1)
+            //what direction to step in x, y direction(either +1 or -1)
             int stepX;
             int stepY;
-            int hit = 0; //was there a wall hit?
-            int side; //was a NS or a EW wall hit?
+            boolean hit = false; //was there a wall hit?
+            int side = 0;        //direction of wall hit - NS or EW
             //calculate step and initial sideDist
             if(rayDirX < 0){
                 stepX = -1;
-                sideDistX = (player->getPosX() - mapX) * deltaDistX;
+                sideDistX = (team.getPosX() - mapX) * deltaDistX;
             }
             else{
                 stepX = 1;
-                sideDistX = (mapX + 1.0 - player->getPosX()) * deltaDistX;
+                sideDistX = (mapX + 1.0 - team.getPosX()) * deltaDistX;
             }
             if(rayDirY < 0){
                 stepY = -1;
-                sideDistY = (player->getPosY() - mapY) * deltaDistY;
+                sideDistY = (team.getPosY() - mapY) * deltaDistY;
             }
             else{
                 stepY = 1;
-                sideDistY = (mapY + 1.0 - player->getPosY()) * deltaDistY;
+                sideDistY = (mapY + 1.0 - team.getPosY()) * deltaDistY;
             }
-            //perform DDA
-            while (hit == 0){
-                //jump to next map square, OR in x-direction, OR in y-direction
+            //perform DDA - ray casting algorithm
+            while(!hit){
+                //jump to next map square in x-dir OR y-dir
                 if(sideDistX < sideDistY){
                     sideDistX += deltaDistX;
                     mapX += stepX;
@@ -97,37 +95,35 @@ public class GameWindow extends JFrame {
                     mapY += stepY;
                     side = 1;
                 }
-                //Check if ray has hit a wall
-                if(map_vect[mapX][mapY] > 0) hit = 1;
+                if(map.getTileWall(mapX, mapY) > 0) hit = true;
             }
-            //Calculate distance projected on camera direction (Euclidean distance will give fisheye effect!)
+            //Calculate non-euclidean distance between player and the ray
             double perpWallDist;
-            if(side == 0) perpWallDist = (mapX - player->getPosX() + (1 - stepX) / 2) / rayDirX;
-            else          perpWallDist = (mapY - player->getPosY() + (1 - stepY) / 2) / rayDirY;
+            if(side == 0) perpWallDist = (mapX - team.getPosX() + (1.0 - stepX) / 2) / rayDirX;
+            else          perpWallDist = (mapY - team.getPosY() + (1.0 - stepY) / 2) / rayDirY;
             //SET THE ZBUFFER FOR THE SPRITE CASTING
-            ZBuffer[x] = perpWallDist; //perpendicular distance is used
-            //Calculate height of line to draw on screen
-            int lineHeight = (int)(casterHeight / perpWallDist);
+            ZBuffer[stripeX] = perpWallDist; //perpendicular distance is used
+            //calculate height of line that should be drawn on the screen
+            int lineHeight = (int)((double)this.getHeight() / perpWallDist);
             //calculate lowest and highest pixel to fill in current stripe
-            int drawStart = -lineHeight / 2 + casterHeight / 2;
+            int drawStart = this.getHeight() / 2 - lineHeight / 2;
+            graphics.drawLine(stripeX, drawStart, stripeX, drawStart + lineHeight);
             //texturing calculations
-            int texNum = map_vect[mapX][mapY] - 1;          //texture number
-            double wallX;                                   //where exactly the wall was hit
-            if (side == 0) wallX = player->getPosY() + perpWallDist * rayDirY;
-            else           wallX = player->getPosX() + perpWallDist * rayDirX;
-            wallX -= std::floor(wallX);
-            int textureX = int(wallX * texWidth);           //x coordinate on the texture
-            //Drawing
-            sf::Sprite lineSprite(resManager->wallTexturesPx[texNum][textureX]);
-            lineSprite.setScale(1, (double)lineHeight/texHeight); //stretches line of pixels to the bottom
-            lineSprite.move(x, drawStart);
-            target.draw(lineSprite);
+            int texNum = map.getTileWall(mapX, mapY) - 1;          //texture number
+            double wallX;                                          //where exactly the wall was hit
+            if (side == 0) wallX = team.getPosY() + perpWallDist * rayDirY;
+            else           wallX = team.getPosX() + perpWallDist * rayDirX;
+            wallX -= Math.floor(wallX);
+            int textureX = (int)(wallX * Constants.WALL_TEXTURE_WIDTH);           //x coordinate on the texture
+            Image texImg = resourceManager.wallImages[texNum];
+            //image, X1, Y1, X2, Y2, SubX1, SubY1, SubX2, SubY2, observer
+            graphics.drawImage(texImg, stripeX, drawStart, stripeX + 1, drawStart + lineHeight,
+                    textureX, 0, textureX + 1, Constants.WALL_TEXTURE_HEIGHT, this);
         }
-         */
-
-
 
         graphics.dispose();
         bufferStrategy.show();
     }
+
+
 }
