9800bac9415c62bff253c11682d6b328b5412a35
mikzur <mikzur4@gmail.com>
2021-01-08 01:26:51 +0100

    add constants class with static fields

---
+++ src/dungeoncrawler/Constants.java
@@ -0,0 +1,11 @@
+package dungeoncrawler;
+
+
+public class Constants {
+    public static final int SCR_WIDTH = 1000;
+    public static final int SCR_HEIGHT = 800;
+    public static final int WALL_TEXTURES_TYPES = 8;
+    public static final int WALL_TEXTURE_WIDTH = 32;
+    public static final int WALL_TEXTURE_HEIGHT = 32;
+    public static final double CAMERA_ANGLE = Math.PI / 2;
+}
