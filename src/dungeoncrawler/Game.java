b0ad0bd993ffe304730d27ff8e3dcd3b997663fe
mikzur <mikzur4@gmail.com>
2021-01-08 01:31:24 +0100

    implement Team into Game class

--- src/dungeoncrawler/Game.java
+++ src/dungeoncrawler/Game.java
@@ -1,26 +1,32 @@
 package dungeoncrawler;
 
+import dungeoncrawler.gameObjects.Input;
+import dungeoncrawler.gameObjects.Team;
+import dungeoncrawler.gameObjects.TeamController;
+
 import java.awt.*;
 
 public class Game {
 
     private GameWindow gameWindow;
-    private Rectangle rectangle;
+    private Map map;
+    private Team team;
+    private double[] ZBuffer;
+    private Input input;
 
     public Game(int width, int height){
-        gameWindow = new GameWindow(width, height);
-        rectangle = new Rectangle(0, 0, 50, 50);
+        input = new Input();
+        gameWindow = new GameWindow(width, height, input);
+        ZBuffer = new double[gameWindow.getWidth()];
+        map = new Map("data/maps/temp_map.txt");
+        team = new Team(2, 2, 1, 0, Constants.CAMERA_ANGLE, new TeamController(input));
     }
 
     public void update(){
-
+        team.update(map);
     }
 
     public void render(){
-        gameWindow.render(this);
-    }
-
-    public Rectangle getRectangle() {
-        return rectangle;
+        gameWindow.renderWorld(team, map, ZBuffer);
     }
 }
