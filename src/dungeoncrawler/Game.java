77179a11aaac8b4fd613460a7c443dc201adc5db
mikzur <mikzur4@gmail.com>
2021-01-08 22:00:38 +0100

    move ZBuffer into GameWindow, add entities arraylist

--- src/dungeoncrawler/Game.java
+++ src/dungeoncrawler/Game.java
@@ -1,25 +1,35 @@
 package dungeoncrawler;
 
-import dungeoncrawler.gameObjects.Input;
-import dungeoncrawler.gameObjects.Team;
-import dungeoncrawler.gameObjects.TeamController;
+import dungeoncrawler.gameobjects.Entity;
+import dungeoncrawler.gameobjects.enemy.Skeleton;
+import dungeoncrawler.iohandler.Input;
+import dungeoncrawler.gameobjects.Team;
+import dungeoncrawler.iohandler.TeamController;
 
-import java.awt.*;
+import java.util.ArrayList;
 
 public class Game {
 
     private GameWindow gameWindow;
     private Map map;
     private Team team;
-    private double[] ZBuffer;
+
     private Input input;
+    private ArrayList<Entity> entities;
 
     public Game(int width, int height){
         input = new Input();
         gameWindow = new GameWindow(width, height, input);
-        ZBuffer = new double[gameWindow.getWidth()];
+
         map = new Map("data/maps/temp_map.txt");
         team = new Team(2, 2, 1, 0, Constants.CAMERA_ANGLE, new TeamController(input));
+
+        entities = new ArrayList<>();
+
+        entities.add(new Skeleton(4, 2));
+        entities.add(new Skeleton(4, 3));
+        entities.add(new Skeleton(4, 4));
+        entities.add(new Skeleton(4, 5));
     }
 
     public void update(){
@@ -27,6 +37,6 @@ public class Game {
     }
 
     public void render(){
-        gameWindow.renderWorld(team, map, ZBuffer);
+        gameWindow.render(team, map, entities);
     }
 }
